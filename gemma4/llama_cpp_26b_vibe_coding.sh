0f6396ae274cadd0054a2775b415f6ed83182ea1
Phong Ha <realphongha@gmail.com>
2026-07-16 04:28:09 +0000

    new gemma4 chat template

--- gemma4/llama_cpp_26b_vibe_coding.sh
+++ gemma4/llama_cpp_26b_vibe_coding.sh
@@ -12,9 +12,9 @@ docker run --gpus all --rm -it \
     $image \
     -hf $model --no-mmproj \
     --host 0.0.0.0 --port $port \
-    -c 65536 -np 1 -b 4096 -ub 4096 -fa on --mlock --threads 8 --n-gpu-layers 999 \
+    -c 65536 -np 1 -b 8192 -ub 2048 -fa on --mlock --threads 8 --n-gpu-layers 999 \
     --cache-type-k q8_0 --cache-type-v q8_0 \
     --temperature 1.0 --top_p 0.95 --top_k 64 \
     --chat-template-kwargs '{"enable_thinking": false}' \
-    --jinja --chat-template-file /gemma4/chat_template_gemma4.fixed.jinja
+    --jinja --chat-template-file /gemma4/chat_template_26b_31b.jinja
 
