0bbcb4bf730f4a9a73f69d279ceeaa8a89a3cc6f
Phong Ha <realphongha@gmail.com>
2026-07-22 03:29:22 +0000

    refactor + update configs

--- gemma4/llama_cpp_26b_vibe.sh
+++ gemma4/llama_cpp_26b_vibe.sh
@@ -1,5 +1,5 @@
 image=llama-cpp
-port=8003
+port=8002
 model=unsloth/gemma-4-26B-A4B-it-GGUF:MXFP4_MOE
 docker run --gpus all --rm -it \
     --ulimit memlock=-1:-1 \
@@ -12,9 +12,9 @@ docker run --gpus all --rm -it \
     $image \
     -hf $model --no-mmproj \
     --host 0.0.0.0 --port $port \
-    -c 131072 -np 2 -cb -b 8192 -ub 2048 -fa on --mlock --threads 8 --n-gpu-layers 999 \
+    -c 262144 -np 2 -cb -b 8192 -ub 2048 -fa on --mlock --threads 8 --n-gpu-layers 999 \
     --cache-type-k q8_0 --cache-type-v q8_0 \
     --temperature 1.0 --top_p 0.95 --top_k 64 \
     --spec-type draft-mtp --spec-draft-n-max 2 \
-    --chat-template-kwargs '{"enable_thinking": false}' \
     --jinja --chat-template-file /gemma4/chat_template_26b_31b.jinja
+    --chat-template-kwargs '{"enable_thinking": false}' \
--- qwen3_6/llama_cpp_27b_vibe.sh
+++ qwen3_6/llama_cpp_27b_vibe.sh
@@ -12,8 +12,8 @@ docker run --gpus all --rm -it \
     $image \
     -hf $model --no-mmproj \
     --host 0.0.0.0 --port $port \
-    -c 131072 -np 2 -cb -fa on --mlock --threads 16 --n-gpu-layers 999 \
-    -b 8192 -ub 2048 --cache-type-k q4_0 --cache-type-v q4_0 \
-    --temperature 0.7 --top_p 0.8 --top_k 20 --min_p 0.0 --presence_penalty 1.5 --repeat_penalty 1.0 \
+    -c 262144 -np 1 -fa on --mlock --threads 16 --n-gpu-layers 999 \
+    -b 8192 -ub 2048 --cache-type-k q8_0 --cache-type-v q8_0 \
+    --temperature 0.7 --top_p 0.80 --top_k 20 --min_p 0.0 --presence_penalty 1.5 --repeat_penalty 1.0 \
     --spec-type draft-mtp --spec-draft-n-max 2 \
     --chat-template-kwargs '{"enable_thinking": false}'
