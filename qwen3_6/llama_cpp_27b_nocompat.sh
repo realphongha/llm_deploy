40f6814a85f1e018aad0776f7c618fa11ac6bde5
Phong Ha <realphongha@gmail.com>
2026-07-25 17:07:43 +0700

    add nocompatt

---
+++ qwen3_6/llama_cpp_27b_nocompat.sh
@@ -0,0 +1,22 @@
+image=llama-cpp
+port=8002
+model=unsloth/Qwen3.6-27B-MTP-GGUF:UD-Q4_K_XL
+# --ulimit memlock=-1:-1 \
+docker run --gpus '"device=1"' --rm -it \
+    -v ~/.cache:/root/.cache \
+    --env "HF_TOKEN=$HF_TOKEN" \
+    --env "LD_LIBRARY_PATH=" \
+    -p $port:$port \
+    --network=host \
+    --name llama-cpp-qwen3.6 \
+    $image \
+    -hf $model \
+    --host 0.0.0.0 --port $port \
+    -c 262144 -np 1 -fa on --threads 16 --n-gpu-layers 999 \
+    -b 8192 -ub 2048 --cache-type-k q4_0 --cache-type-v q4_0 \
+    --temperature 0.7 --top_p 0.80 --top_k 20 --min_p 0.0 --presence_penalty 1.5 --repeat_penalty 1.0 \
+    --spec-type draft-mtp --spec-draft-n-max 2 \
+    --chat-template-kwargs '{"enable_thinking": false}' \
+    --image-min-tokens 1024
+    # --mlock
+    # --ulimit memlock=-1:-1 \
