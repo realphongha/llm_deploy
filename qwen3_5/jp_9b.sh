ad45e4bba56d77d05013d7ddfdd11aec6db22761
Phong Ha <realphongha@gmail.com>
2026-07-28 02:12:11 +0000

    change qk cache type to q8_0

--- qwen3_5/jp_9b.sh
+++ qwen3_5/jp_9b.sh
@@ -15,7 +15,7 @@ docker run --rm \
     -hf $model \
     --host 0.0.0.0 --port 8002 \
     -fa on --mlock --threads 8 --n-gpu-layers 999 \
-    -b 8192 -ub 2048 --cache-type-k q4_0 --cache-type-v q4_0 \
+    -b 8192 -ub 2048 --cache-type-k q8_0 --cache-type-v q8_0 \
     -np 1 -c 65536 \
     --temperature 0.7 --top_p 0.8 --top_k 20 --min_p 0.0 --presence_penalty 1.5 --repeat_penalty 1.0 \
     --chat-template-kwargs '{"enable_thinking": false}' \
--- qwen3_6/llama_cpp_27b_nocompat.sh
+++ qwen3_6/llama_cpp_27b_nocompat.sh
@@ -13,7 +13,7 @@ docker run --gpus '"device=1"' --rm -it \
     -hf $model \
     --host 0.0.0.0 --port $port \
     -c 262144 -np 1 -fa on --threads 16 --n-gpu-layers 999 \
-    -b 8192 -ub 2048 --cache-type-k q4_0 --cache-type-v q4_0 \
+    -b 8192 -ub 2048 --cache-type-k q8_0 --cache-type-v q8_0 \
     --temperature 0.7 --top_p 0.80 --top_k 20 --min_p 0.0 --presence_penalty 1.5 --repeat_penalty 1.0 \
     --spec-type draft-mtp --spec-draft-n-max 2 \
     --chat-template-kwargs '{"enable_thinking": false}' \
