e5064098e1dc891b49833a43d6881636ab131a90
Phong Ha <realphongha@gmail.com>
2026-08-04 14:34:17 +0700

    update q36 27b params

--- qwen3_6/llama_cpp_27b_vibe_nocompat.sh
+++ qwen3_6/llama_cpp_27b_vibe_nocompat.sh
@@ -2,7 +2,7 @@ image=llama-cpp
 port=8002
 model=unsloth/Qwen3.6-27B-MTP-GGUF:UD-Q4_K_XL
 # --ulimit memlock=-1:-1 \
-docker run --gpus '"device=1"' --rm -it \
+docker run --gpus '"device=0"' --rm -it \
     -v ~/.cache:/root/.cache \
     --env "HF_TOKEN=$HF_TOKEN" \
     --env "LD_LIBRARY_PATH=" \
@@ -14,7 +14,7 @@ docker run --gpus '"device=1"' --rm -it \
     --host 0.0.0.0 --port $port \
     -c 262144 -np 1 -fa on --threads 16 --n-gpu-layers 999 \
     -b 8192 -ub 2048 --cache-type-k q8_0 --cache-type-v q8_0 \
-    --temperature 1.0 --top_p 0.95 --top_k 20 --min_p 0.0 --presence_penalty 0.0 --repeat_penalty 1.0 --chat-template-kwargs '{"enable_thinking": true}' \
+    --temperature 0.6 --top_p 0.95 --top_k 20 --min_p 0.0 --presence_penalty 0.0 --repeat_penalty 1.0 --chat-template-kwargs '{"enable_thinking": true}' \
     --spec-type draft-mtp --spec-draft-n-max 2 \
     # --mlock
     # --ulimit memlock=-1:-1 \
