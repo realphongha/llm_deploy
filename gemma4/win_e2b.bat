2f022e97f3583f05cc9f736a17b764b05361951b
Phong Ha <realphongha@gmail.com>
2026-08-06 20:13:36 +0700

    update gemma4 windows

--- gemma4/win_e2b.bat
+++ gemma4/win_e2b.bat
@@ -1,11 +1,11 @@
 @echo off
 
 llama-server ^
-    -hf unsloth/gemma-4-E2B-it-GGUF:Q4_K_M ^
+    -hf unsloth/gemma-4-E2B-it-qat-GGUF:UD-Q4_K_XL ^
     --host 0.0.0.0 --port 8002 ^
     -fa on --mlock --threads 8 --n-gpu-layers 999 ^
-    -b 8192 -ub 2048 --cache-type-k q4_0 --cache-type-v q4_0 ^
-    -np 1 -c 65536 -cb ^
+    -b 8192 -ub 2048 --cache-type-k bf16 --cache-type-v bf16 ^
+    -np 1 -c 16384 ^
     --temperature 1.0 --top_p 0.95 --top_k 64 ^
     --reasoning off ^
     --jinja --chat-template-file chat_template_e2b.jinja ^
--- gemma4/win_e4b.bat
+++ gemma4/win_e4b.bat
@@ -1,15 +1,15 @@
 @echo off
 
 llama-server ^
-    -hf unsloth/gemma-4-E4B-it-GGUF:Q4_K_M ^
+    -hf unsloth/gemma-4-E4B-it-qat-GGUF:UD-Q4_K_XL ^
     --host 0.0.0.0 --port 8002 ^
-    -fa on --mlock --threads 8 --n-gpu-layers 999 ^
-    -b 8192 -ub 2048 --cache-type-k q4_0 --cache-type-v q4_0 ^
-    -np 2 -c 131072 -cb ^
+    -fa on --threads 8 --n-gpu-layers 999 ^
+    -b 8192 -ub 2048 --cache-type-k bf16 --cache-type-v bf16 ^
+    -np 1 -c 65536 -cb ^
     --temperature 1.0 --top_p 0.95 --top_k 64 ^
     --reasoning off ^
     --jinja --chat-template-file chat_template_e4b.jinja ^
     --spec-type draft-mtp --spec-draft-n-max 2 ^
-    --image-max-tokens 1120
+    --image-max-tokens 560
 
 pause
