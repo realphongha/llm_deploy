2de6fed16e66b3015a9adfb4a5edd632ef7a78df
Phong Ha <realphongha@gmail.com>
2026-08-24 09:29:17 +0700

    add mmproj for qwen3.8 27b

--- qwen3_8/llama_cpp_27b_vibe.sh
+++ qwen3_8/llama_cpp_27b_vibe.sh
@@ -8,10 +8,10 @@ docker run --gpus '"device=2"' --rm -it \
     -p $port:$port \
     --name llama-cpp-qwen3.8-27b \
     $image \
-    -hf $model --no-mmproj \
+    -hf $model \
     --host 0.0.0.0 --port $port \
     -c 131072 -np 1 -cb -fa on --mlock --threads 4 --n-gpu-layers 999 \
-    -b 2048 -ub 2048 --cache-type-k q8_0 --cache-type-v q8_0 \
+    -b 512 -ub 512 --cache-type-k q8_0 --cache-type-v q8_0 \
     --spec-type draft-mtp --spec-draft-n-max 2 -ctkd q8_0 -ctvd q8_0 \
     --jinja \
     --temperature 1.0 --top_p 0.95 --top_k 20 --min_p 0.0 --presence_penalty 0.0 --repeat_penalty 1.0 --chat-template-kwargs '{"enable_thinking": true, "preserve_thinking": true, "reasoning_effort": "low"}' \
