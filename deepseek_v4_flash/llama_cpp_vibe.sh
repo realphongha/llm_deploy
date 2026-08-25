fe91204ab52c046f0bd4a13a0cfaae51ba3e71f1
Phong Ha <phong.ha@awl.com.vn>
2026-08-25 16:47:21 +0700

    better kv quant for jetpack6

--- deepseek_v4_flash/llama_cpp_vibe.sh
+++ deepseek_v4_flash/llama_cpp_vibe.sh
@@ -12,7 +12,7 @@ docker run --gpus all --rm -it \
     -hf $model --no-mmproj \
     --host 0.0.0.0 --port $port \
     -c 65536 -np 1 -b 4096 -ub 1024 --mlock --threads 16 --n-gpu-layers 999 \
-    --cache-type-k q4_0 --cache-type-v q4_0 \
+    --cache-type-k q8_0 --cache-type-v q8_0 \
     -fa on \
     --temperature 1.0 --top_p 1.0 --min_p 0.0 \
     --chat-template-kwargs '{"enable_thinking": false}'
--- qwen3_6/jp_27b_vibe.sh
+++ qwen3_6/jp_27b_vibe.sh
@@ -15,7 +15,7 @@ docker run --rm \
     -hf $model --no-mmproj \
     --host 0.0.0.0 --port 8002 \
     -fa on --mlock --threads 8 --n-gpu-layers 999 \
-    -b 4096 -ub 4096 --cache-type-k q4_0 --cache-type-v q4_0 \
+    -b 4096 -ub 4096 --cache-type-k q8_0 --cache-type-v q8_0 \
     -np 1 -c 65536 \
     --temperature 0.7 --top_p 0.8 --top_k 20 --min_p 0.0 --presence_penalty 1.5 --repeat_penalty 1.0 \
     --chat-template-kwargs '{"enable_thinking":false}' \
